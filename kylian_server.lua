a0de47968b59ecdbc8bbe61c8b7564d832272f49
LozeKylian <66750292+LozeKylian@users.noreply.github.com>
2020-07-25 12:48:19 +0200

    Ano
    ESX Nil

--- kylian_server.lua
+++ kylian_server.lua
@@ -31,26 +31,25 @@ RegisterCommand('ano', function(source, args, rawCommand)
 end, false)
 
 
-    RegisterCommand('lspd', function(source, args, rawCommand)
-        local _source = source
-        local xPlayer = ESX.GetPlayerFromId(_source)
+RegisterCommand('lspd', function(source, args, rawCommand)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
     if xPlayer.job.name == "police" then
         local src = source
         local msg = rawCommand:sub(5)
         local args = msg
         if player ~= false then
-                local name = GetPlayerName(source)
-                local xPlayers	= ESX.GetPlayers()
-            for i=1, #xPlayers, 1 do
-                local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-                TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'LSPD', '~b~Annonce LSPD', ''..msg..'', 'CHAR_ABIGAIL', 0)
-            end
-        else
-            TriggerClientEvent('esx:showAdvancedNotification', _source, 'Avertisement', '~b~Tu n\'pas' , 'policier pour faire cette commande', 'CHAR_ABIGAIL', 0)
+            local name = GetPlayerName(source)
+            local xPlayers	= ESX.GetPlayers()
+        for i=1, #xPlayers, 1 do
+            local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+            TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'LSPD', '~b~Annonce LSPD', ''..msg..'', 'CHAR_ABIGAIL', 0)
         end
+    else
+        TriggerClientEvent('esx:showAdvancedNotification', _source, 'Avertisement', '~b~Tu n\'pas' , 'policier pour faire cette commande', 'CHAR_ABIGAIL', 0)
+    end
     else
     TriggerClientEvent('esx:showAdvancedNotification', _source, 'Avertisement', '~b~Tu n\'est pas' , 'policier pour faire cette commande', 'CHAR_ABIGAIL', 0)
     end
-
-    end, false)
+ end, false)
     
